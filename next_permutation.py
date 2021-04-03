609295e04c23d726248923d5d2d2585c9359c04d
behrooz2011 <bmmm1370@gmail.com>
2021-04-03 09:26:00 -0400

    Using while

--- next_permutation.py
+++ next_permutation.py
@@ -34,3 +34,23 @@ print(nextPerm([1,2,3]))
 print(nextPerm([2]))
 print(nextPerm([2,3,6,5,1]))
 print(nextPerm([5,4,0]))
+
+
+#different approach
+class Solution:
+	def nextPermutation(self, nums: List[int]) -> None:
+		index = len(nums) - 1
+		while index - 1 >= 0 and nums[index] <= nums[index - 1]:
+			index -= 1
+			
+		if index == 0:
+			nums.reverse()
+			return
+
+		greater = index
+		
+		while greater + 1 < len(nums) and nums[greater + 1] > nums [index - 1]:
+			greater += 1
+		
+		nums[greater], nums[index - 1] = nums[index - 1], nums[greater]
+		nums[index :] = nums[index :][::-1]
