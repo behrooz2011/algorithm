20e21f9be2eaefe12a6118eb1795d8fc488acd76
behrooz2011 <bmmm1370@gmail.com>
2021-04-07 12:57:41 -0400

    string compression complexity o(n)

--- string_compression.py
+++ string_compression.py
@@ -24,3 +24,26 @@ stringCompression("permanent")
 stringCompression("hello")
 stringCompression("baby")
 stringCompression("wwwwaaab")
+
+#another one acooding to the book's example
+print('Hello, world!')
+def compress_string(string):
+    compressed = []
+    counter = 0
+
+    for i in range(len(string)):  # noqa
+        if i != 0 and string[i] != string[i - 1]:
+            compressed.append(string[i - 1] + str(counter))
+            counter = 0
+        counter += 1
+
+    # add last repeated character
+    if counter:
+        compressed.append(string[-1] + str(counter))
+
+    # returns original string if compressed string isn't smaller
+    print("this is the result:","".join(compressed))
+    return min(string, "".join(compressed), key=len)
+print(compress_string("hello"))
+print(compress_string("permanent"))
+print(compress_string("aabcccccaaa"))
